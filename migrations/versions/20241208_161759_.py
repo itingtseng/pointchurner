13dee4a33f142fc0c26393809c7ecb7416479805
itingtseng <42088639+itingtseng@users.noreply.github.com>
2024-12-08 16:18:56 -0500

    Drop schema flask_schema cascade

--- migrations/versions/20241208_161759_.py
+++ migrations/versions/20241208_161759_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: aeb9059b7a12
+Revision ID: 4412bea4aca0
 Revises: 
-Create Date: 2024-12-08 15:15:39.491630
+Create Date: 2024-12-08 16:17:59.864906
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'aeb9059b7a12'
+revision = '4412bea4aca0'
 down_revision = None
 branch_labels = None
 depends_on = None
